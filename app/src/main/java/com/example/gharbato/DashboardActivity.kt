a9b1ff8169d0218a6bf56213d0f4d8bda71c4a3b
ManjilDadaa <dev.manjil.c@gmail.com>
2025-12-02 18:52:48 +0545

    Added search Widget

--- app/src/main/java/com/example/gharbato/DashboardActivity.kt
+++ app/src/main/java/com/example/gharbato/DashboardActivity.kt
@@ -19,21 +19,37 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gharbato.ui.theme.Green
 
 class DashboardActivity : ComponentActivity() {
@@ -48,6 +64,9 @@ class DashboardActivity : ComponentActivity() {
 
 @Composable
 fun DashboardBody(){
+
+    var search by remember { mutableStateOf("") }
+
     Scaffold { padding ->
         Column(
             modifier = Modifier
@@ -133,12 +152,54 @@ fun DashboardBody(){
                     )
                 }
             }
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp)
+                    .height(48.dp),
+
+                value = search,
+                onValueChange = { data ->
+                    search = data
+                },
+
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                ),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color.Gray.copy(0.2f),
+                    unfocusedContainerColor = Color.Gray.copy(0.2f),
+                    unfocusedBorderColor = Color.Gray.copy(0.1f),
+                    focusedBorderColor = Green,
+                ),
+                placeholder = {
+                    Text("Search Properties...",
+                        style = TextStyle(
+                            fontSize = 14.sp
+                        )
+                    )
+                },
+
+                leadingIcon = {
+                    Icon(painter = painterResource(R.drawable.outline_search_24),
+                        contentDescription = null
+                    )
+                },
+
+                shape = RoundedCornerShape(10.dp),
+
+            )
+
         }
     }
 }
 
+
+
+
 @Composable
-@Preview
+ @Preview
 fun DashboardPreview(){
     DashboardBody()
 }
