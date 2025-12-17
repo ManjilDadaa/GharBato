f6190a9eed91b3e78ef79f5411f11880bb1d4dab
ManjilDadaa <dev.manjil.c@gmail.com>
2025-12-17 19:34:32 +0545

    Added 3 listing features : sell, rent, book

---
+++ app/src/main/java/com/example/gharbato/view/PhotosContentScreen.kt
@@ -0,0 +1,9 @@
+package com.example.gharbato.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PhotosContentScreen() {
+    Text("This is Step 3 content")
+}
