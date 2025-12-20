0ebf0f9c2cb71e43f74801a4dbe519df7ade32f5
bishowdip <bishodip123@gmail.com>
2025-12-20 23:11:37 +0545

    structured in MVVM model

--- app/src/main/java/com/example/gharbato/view/SearchScreen.kt
+++ app/src/main/java/com/example/gharbato/view/SearchScreen.kt
@@ -1,4 +1,4 @@
-package com.example.gharbato
+package com.example.gharbato.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -15,7 +15,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
